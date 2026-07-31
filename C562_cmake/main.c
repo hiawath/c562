7d7097858ebfce699d100c7c4325fe32fae859a5
hanki <kihan@snut.ac.kr>
2026-07-31 22:14:55 +0900

    C562 init

---
+++ C562_cmake/main.c
@@ -0,0 +1,59 @@
+/**
+  ******************************************************************************
+  * file           : main.c
+  * brief          : Main program body
+  *                  Calls target system initialization then loop in main.
+  ******************************************************************************
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32c562xx.h"
+#include "stm32c5xx_hal.h"
+#include "stm32c5xx_hal_gpio.h"
+
+#include "appMain.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private functions prototype -----------------------------------------------*/
+
+/**
+  * brief:  The application entry point.
+  * retval: none but we specify int to comply with C99 standard
+  */
+int main(void)
+{
+  /** System Init: this code placed in targets folder initializes your system.
+    * It calls the initialization (and sets the initial configuration) of the peripherals.
+    * You can use STM32CubeMX to generate and call this code or not in this project.
+    * It also contains the HAL initialization and the initial clock configuration.
+    */
+  if (mx_system_init() != SYSTEM_OK)
+  {
+    return (-1);
+  }
+  else
+  {
+    /*
+      * You can start your application code here
+      */
+    appMain();
+    while (1) {
+
+
+
+    }
+  }
+} /* end main */
+
---
+++ C562_cmake/user_modifiable/Application/src/appMain.c
@@ -0,0 +1,10 @@
+#include "main.h"
+void appMain(){
+
+    while(1){
+        HAL_GPIO_TogglePin(HAL_GPIOA, HAL_GPIO_PIN_5);
+        HAL_Delay(100);
+}
+
+    }
+
