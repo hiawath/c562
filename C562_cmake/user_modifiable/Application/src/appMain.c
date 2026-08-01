ccc95f867b6f3d5411b588c2691709ea82b8bd2b
Andrew Han <kihan@snut.ac.kr>
2026-08-01 20:25:59 +0900

    button 입력 PC13  추가

--- C562_cmake/user_modifiable/Application/src/appMain.c
+++ C562_cmake/user_modifiable/Application/src/appMain.c
@@ -1,10 +1,11 @@
 #include "main.h"
-void appMain(){
+#include "stm32c5xx_hal_gpio.h"
+void appMain() {
 
-    while(1){
-        HAL_GPIO_TogglePin(HAL_GPIOA, HAL_GPIO_PIN_5);
-        HAL_Delay(100);
-}
-
+  while (1) {
+    if (HAL_GPIO_ReadPin(HAL_GPIOC, HAL_GPIO_PIN_13) == HAL_GPIO_PIN_SET) {
+      HAL_GPIO_TogglePin(HAL_GPIOA, HAL_GPIO_PIN_5);
+      HAL_Delay(1000);
     }
-
+  }
+}
